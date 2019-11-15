402063a005f8734cb809117dba5cebdd3b5f295c
Mikhail_Rusakovich <Mikhail_Rusakovich@epam.com>
2019-11-15 12:50:01 +0300

    - tampering example modify  - readme edit

--- src/test/java/com/github/rusakovichma/security/aes/cbc/TamperingAttack.java
+++ src/test/java/com/github/rusakovichma/security/aes/cbc/TamperingAttack.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TamperingAttack {
+class TamperingAttack {
 
     private final DecryptService decryptService = SettingsContainer.DECRYPT_SERVICE;
 
     //Text: {"id": 12345, "balance": 200}
-    //BxROBBxG/SesEslIqX7Kz3PVXBKOWuPYh4iDJjLxwys=
+    //BxROBBxG/SesEslIqX7Kz{3PVXBKOWuPYh4iDJjLxwys=}
     //Text: {"id": 12345, "balance": 900}
-    //BxROBBxG/SesEslIqX7KzxWjHN49c/6nDx7fRKIzsAU=
+    //BxROBBxG/SesEslIqX7Kz{xWjHN49c/6nDx7fRKIzsAU=}
     @Test
-    void  encryptedDataTamperingAttack() throws Exception{
+    void  encryptedDataTamperingAttack() {
         //encrypted - {"id": 12345, "balance": 200}
         String encryptedPrimary = "BxROBBxG/SesEslIqX7Kz3PVXBKOWuPYh4iDJjLxwys=";
 
